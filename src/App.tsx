65fce7f73a4216332c85836e581bcf35b8cdca02
Roebling Labs <150849464+roeblinglabs@users.noreply.github.com>
2024-11-30 18:53:50 -0500

    Update App.tsx
    Reverting to working code from before I tried and failed to add maps and terrain.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import "./App.scss";
 
 import type { ScreenViewport } from "@itwin/core-frontend";
-import { FitViewTool, IModelApp, TileAdmin, StandardViewId, Marker, DecorateContext } from "@itwin/core-frontend";
+import { FitViewTool, IModelApp, StandardViewId, Marker, DecorateContext } from "@itwin/core-frontend";
 import { FillCentered } from "@itwin/core-react";
+import { ECSchemaRpcInterface } from "@itwin/ecschema-rpcinterface-common";
 import { ProgressLinear } from "@itwin/itwinui-react";
 import {
   MeasurementActionToolbar,
   MeasureTools,
+  MeasureToolsUiItemsProvider,
 } from "@itwin/measure-tools-react";
-import { PropertyGridManager } from "@itwin/property-grid-react";
-import { TreeWidget } from "@itwin/tree-widget-react";
-import { Viewer } from "@itwin/web-viewer-react";
+import {
+  AncestorsNavigationControls,
+  CopyPropertyTextContextMenuItem,
+  PropertyGridManager,
+  PropertyGridUiItemsProvider,
+  ShowHideNullValuesSettingsMenuItem,
+} from "@itwin/property-grid-react";
+import {
+  CategoriesTreeComponent,
+  createTreeWidget,
+  ModelsTreeComponent,
+  TreeWidget,
+} from "@itwin/tree-widget-react";
+import {
+  useAccessToken,
+  Viewer,
+  ViewerContentToolsProvider,
+  ViewerNavigationToolsProvider,
+  ViewerPerformance,
+  ViewerStatusbarItemsProvider,
+} from "@itwin/web-viewer-react";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import { Point3d } from "@itwin/core-geometry";
 import { Auth } from "./Auth";
+import { history } from "./history";
+import { getSchemaContext, unifiedSelectionStorage } from "./selectionStorage";
 
 export class VideoCameraMarker extends Marker {
   constructor(location: Point3d, size: { x: number; y: number }, label: string, onClick: () => void) {
@@ -35,21 +57,62 @@ export class VideoCameraMarker extends Marker {
   }
 }
 
+export class DisplacementSensorMarker extends Marker {
+  constructor(location: Point3d, size: { x: number; y: number }, label: string, onClick: () => void) {
+    super(location, size);
+
+    this.title = `Displacement Sensor: ${label}`;
+    this.setImageUrl("/images/icons8-sensor-96.png");
+    this.label = label;
+    this.labelOffset = { x: 0, y: 30 };
+
+    this.onMouseButton = (ev) => {
+      if (ev.button === 0) {
+        onClick(); // Call the provided onClick function
+        return true;
+      }
+      return false;
+    };
+  }
+}
+
+export class MicroscopeMarker extends Marker {
+  constructor(location: Point3d, size: { x: number; y: number }, label: string, onClick: () => void) {
+    super(location, size);
+
+    this.title = `Microscope: ${label}`;
+    this.setImageUrl("/images/icons8-microscope-64.png");
+    this.label = label;
+    this.labelOffset = { x: 0, y: 30 };
+
+    this.onMouseButton = (ev) => {
+      if (ev.button === 0) {
+        onClick(); // Call the provided onClick function
+        return true;
+      }
+      return false;
+    };
+  }
+}
+
 const App: React.FC = () => {
   const [iModelId, setIModelId] = useState(process.env.IMJS_IMODEL_ID);
   const [iTwinId, setITwinId] = useState(process.env.IMJS_ITWIN_ID);
-  const [changesetId, setChangesetId] = useState(process.env.IMJS_AUTH_CLIENT_CHANGESET_ID);
+  const [changesetId, setChangesetId] = useState(
+    process.env.IMJS_AUTH_CLIENT_CHANGESET_ID
+  );
   const [showVideo, setShowVideo] = useState(false); // New state to control video display
 
-  const accessToken = Auth.getClient();
+  const accessToken = useAccessToken();
+  const authClient = Auth.getClient();
 
   const login = useCallback(async () => {
     try {
-      await accessToken.signInSilent();
+      await authClient.signInSilent();
     } catch {
-      await accessToken.signIn();
+      await authClient.signIn();
     }
-  }, [accessToken]);
+  }, [authClient]);
 
   useEffect(() => {
     void login();
@@ -57,46 +120,72 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has("iTwinId")) setITwinId(urlParams.get("iTwinId") as string);
-    if (urlParams.has("iModelId")) setIModelId(urlParams.get("iModelId") as string);
-    if (urlParams.has("changesetId")) setChangesetId(urlParams.get("changesetId") as string);
+    if (urlParams.has("iTwinId")) {
+      setITwinId(urlParams.get("iTwinId") as string);
+    }
+    if (urlParams.has("iModelId")) {
+      setIModelId(urlParams.get("iModelId") as string);
+    }
+    if (urlParams.has("changesetId")) {
+      setChangesetId(urlParams.get("changesetId") as string);
+    }
   }, []);
 
-  const cesiumIonToken: string = process.env.CESIUM_ION_TOKEN ?? "";
+  useEffect(() => {
+    let url = `viewer?iTwinId=${iTwinId}`;
+    if (iModelId) url = `${url}&iModelId=${iModelId}`;
+    if (changesetId) url = `${url}&changesetId=${changesetId}`;
+    history.push(url);
+  }, [iTwinId, iModelId, changesetId]);
+
+  const viewCreatorOptions = useMemo(() => {
+    return {
+      viewportConfigurer: (vp: ScreenViewport) => {
+        class MarkerDecorator {
+          private videoMarkers: Marker[];
+          private displacementMarkers: Marker[];
+          private microscopeMarkers: Marker[];
+
+          constructor(videoMarkers: Marker[], displacementMarkers: Marker[], microscopeMarkers: Marker[]) {
+            this.videoMarkers = videoMarkers;
+            this.displacementMarkers = displacementMarkers;
+            this.microscopeMarkers = microscopeMarkers;
+          }
+
+          public decorate(context: DecorateContext): void {
+            this.videoMarkers.forEach((marker) => marker.addDecoration(context));
+            this.displacementMarkers.forEach((marker) => marker.addDecoration(context));
+            this.microscopeMarkers.forEach((marker) => marker.addDecoration(context));
+          }
+        }
+
+        const videoCameraMarkers = [
+          new VideoCameraMarker(new Point3d(-10, 20, 5), { x: 40, y: 40 }, "Shore Camera 1", () => setShowVideo(true)),
+          new VideoCameraMarker(new Point3d(-15, 25, 5), { x: 40, y: 40 }, "Shore Camera 2", () => setShowVideo(true)),
+        ];
+
+        const displacementMarkers = [
+          new DisplacementSensorMarker(new Point3d(0, 0, 10), { x: 40, y: 40 }, "Bridge Girder 1", () => setShowVideo(true)),
+          new DisplacementSensorMarker(new Point3d(5, 0, 10), { x: 40, y: 40 }, "Bridge Girder 2", () => setShowVideo(true)),
+          new DisplacementSensorMarker(new Point3d(10, 0, 10), { x: 40, y: 40 }, "Bridge Girder 3", () => setShowVideo(true)),
+        ];
+
+        const microscopeMarkers = [
+          new MicroscopeMarker(new Point3d(20, 10, 15), { x: 40, y: 40 }, "Microscope 1", () => setShowVideo(true)),
+        ];
+
+        const markerDecorator = new MarkerDecorator(videoCameraMarkers, displacementMarkers, microscopeMarkers);
+        IModelApp.viewManager.addDecorator(markerDecorator);
+      },
+    };
+  }, []);
 
   const onIModelAppInit = useCallback(async () => {
-    IModelApp.startup({
-      tileAdmin: TileAdmin.create({ cesiumIonKey: cesiumIonToken }),
-    });
-
     await TreeWidget.initialize();
     await PropertyGridManager.initialize();
     await MeasureTools.startup();
     MeasurementActionToolbar.setDefaultActionProvider();
-  }, [cesiumIonToken]);
-
-  const viewCreatorOptions = useMemo(() => ({
-    viewportConfigurer: (vp: ScreenViewport) => {
-      class MarkerDecorator {
-        private videoMarkers: Marker[];
-
-        constructor(videoMarkers: Marker[]) {
-          this.videoMarkers = videoMarkers;
-        }
-
-        public decorate(context: DecorateContext): void {
-          this.videoMarkers.forEach((marker) => marker.addDecoration(context));
-        }
-      }
-
-      const videoCameraMarkers = [
-        new VideoCameraMarker(new Point3d(-10, 20, 5), { x: 40, y: 40 }, "Shore Camera 1", () => setShowVideo(true)),
-      ];
-
-      const markerDecorator = new MarkerDecorator(videoCameraMarkers);
-      IModelApp.viewManager.addDecorator(markerDecorator);
-    },
-  }), []);
+  }, []);
 
   return (
     <div className="viewer-container">
@@ -127,7 +216,7 @@ const App: React.FC = () => {
         iTwinId={iTwinId ?? ""}
         iModelId={iModelId ?? ""}
         changeSetId={changesetId}
-        authClient={accessToken}
+        authClient={authClient}
         viewCreatorOptions={viewCreatorOptions}
         enablePerformanceMonitors={true}
         onIModelAppInit={onIModelAppInit}
